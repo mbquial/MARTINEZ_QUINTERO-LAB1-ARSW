228ce586ec8d13322f27d9236c3354cd0d4e0d73
mbquial <pepitotolitis@gmail.com>
2026-01-23 21:36:41 -0500

    feat: add run method logic and getters

--- src/main/java/edu/eci/arsw/blacklistvalidator/HostBlackListThread.java
+++ src/main/java/edu/eci/arsw/blacklistvalidator/HostBlackListThread.java
@@ -1,8 +1,30 @@
 package edu.eci.arsw.blacklistvalidator;
 
+import java.util.ArrayList;
+
+import edu.eci.arsw.spamkeywordsdatasource.HostBlacklistsDataSourceFacade;
+
 public class HostBlackListThread extends Thread{
+    private String host;
+    private int start, end, occurrences;
+    private ArrayList<Integer> foundLists;
+    private HostBlacklistsDataSourceFacade skds;
+
     @Override
     public void run(){
-        
+        for (int i = start; i <= end; i++){
+            if (skds.isInBlackListServer(i, host)){
+                occurrences++;
+                foundLists.add(i);
+            }
+        }
+    }
+
+    public int getOcurrences(){
+        return occurrences;
+    }
+
+    public ArrayList<Integer> getBlackList(){
+        return foundLists;
     }
 }
